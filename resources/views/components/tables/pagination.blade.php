bbc527ac944cba7280295d65ff838c7fc90206f2
nicodwi_k <nicodwika@gmail.com>
2024-03-20 00:50:39 +0700

    fix: pagination threshold

--- resources/views/components/tables/pagination.blade.php
+++ resources/views/components/tables/pagination.blade.php
@@ -1,17 +1,26 @@
-@props(['meta' => null])
+@props(['meta' => null, 'total_pagination_showed' => 10, 'max_current_page_threshold' => 8])
 
 <div class="card-footer">
-    {{-- @dump($meta) --}}
     <div class="d-flex justify-content-between">
+        
         <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
             <x-buttons.pagination-action :meta="$meta" :action="'prev'" :label="'chevron-left'"/>
+            @php
+                $count = ($meta->current_page + ($total_pagination_showed - $max_current_page_threshold)) <= $meta->total_pages ? $total_pagination_showed : $meta->total_pages - ($meta->current_page - $max_current_page_threshold);
+            @endphp
             @for ($i = 1; $i <= $meta->total_pages; $i++)
-                @if ($i <= 10)
-                    <x-buttons.pagination-number :meta="$meta" :number="($meta->current_page >= 7 ? $meta->current_page + $i - 7 : $i)" />
+                @if ($i <= $count)
+                    <x-buttons.pagination-number :meta="$meta" :number="($meta->current_page >= $max_current_page_threshold ? $meta->current_page + $i - $max_current_page_threshold : $i)" />
                 @endif
             @endfor 
-            <x-buttons.pagination-action :meta="$meta" :action="'next'"  :label="'chevron-right'"/>
+            <x-buttons.pagination-action :meta="$meta" :action="'next'" :label="'chevron-right'"/>
         </div>
-        <span>Showing <b>{{ $meta->current_page * $meta?->items_per_page }}</b> of <b>{{ $meta?->total_items }}</b> items</span>
+        <span>
+            Showing 
+                <b>{{ $meta->current_page != $meta->total_pages ? $meta->current_page * $meta?->items_per_page : $meta?->total_items }}</b>
+                    of 
+                <b>{{ $meta?->total_items }}</b> 
+            items
+        </span>
     </div>
 </div>
