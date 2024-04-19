e671dbe6d3cbf6d2539050b68b7d3f9a19f78978
nicodwi_k <nicodwika@gmail.com>
2024-04-19 07:29:23 +0700

    fix: add sidenav mobile

--- resources/views/components/layouts/navbars/auth/nav.blade.php
+++ resources/views/components/layouts/navbars/auth/nav.blade.php
@@ -27,7 +27,7 @@
                     <li class="nav-item dropdown d-flex align-items-center">
                         <livewire:auth.logout lazy />
                     </li>
-                    {{-- <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                    <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
                         <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
                             <div class="sidenav-toggler-inner">
                                 <i class="sidenav-toggler-line"></i>
@@ -35,7 +35,7 @@
                                 <i class="sidenav-toggler-line"></i>
                             </div>
                         </a>
-                    </li> --}}
+                    </li>
                     {{-- <li class="nav-item px-3 d-flex align-items-center">
                         <a href="javascript:;" class="nav-link text-body p-0">
                             <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
