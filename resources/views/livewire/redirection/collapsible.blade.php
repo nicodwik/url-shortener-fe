3b5b40ad150165b0bf4da1eeb478085d180f5a49
nicodwi_k <nicodwika@gmail.com>
2024-04-19 15:28:54 +0700

    .

--- resources/views/livewire/redirection/collapsible.blade.php
+++ resources/views/livewire/redirection/collapsible.blade.php
@@ -62,11 +62,16 @@
         const collapsible = new bootstrap.Collapse('[data-bs-toggle="collapse"]')
 
         function copyToClipboard(shortUrl, el) {
-            navigator.clipboard.writeText(shortUrl);
-            el.innerHTML = '<small>Copied to clipboard!</small>'
-            setTimeout(() => {
-                el.innerHTML = '<i class="fa fa-copy"></i>'
-            }, 3000);
+            navigator.clipboard.writeText(shortUrl)
+                .then(() => {
+                    el.innerHTML = '<small>Copied to clipboard!</small>'
+                    setTimeout(() => {
+                        el.innerHTML = '<i class="fa fa-copy"></i>'
+                    }, 3000);
+                })
+                .catch(() => {
+                    alert("something went wrong");
+                });
         }
     </script>
 @endpush
