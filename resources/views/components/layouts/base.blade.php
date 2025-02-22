837cde9ba8ba140170dc71364fe8ea67b7559b92
nicodwi_k <nicodwika@gmail.com>
2025-02-22 21:45:04 +0700

    .

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}">
-    <link rel="icon" type="image/png" href="{{ asset('/assets/img/favicon.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('/assets/img/favicon-2.ico') }}">
     <title>
-        {{ config('app.name') }} by  {{ config('app.creator') }}
+        {{ config('app.name') }} by {{ config('app.creator') }}
     </title>
     <!-- Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
@@ -15,14 +15,15 @@
     <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
     <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
     <!-- Font Awesome Icons -->
-    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    {{-- <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script> --}}
     <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
     <!-- CSS Files -->
-    {{-- <link id="pagestyle" href="{{ asset('assets/css/soft-ui-dashboard.css') }}?v=1" rel="stylesheet" /> --}}
+    {{--
+    <link id="pagestyle" href="{{ asset('assets/css/soft-ui-dashboard.css') }}?v=1" rel="stylesheet" /> --}}
     <link id="pagestyle" href="{{ asset('guest/assets/css/soft-design-system.min.css') }}?v=1.0.9" rel="stylesheet" />
     <!-- Alpine -->
     {{-- <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script> --}}
-    
+
     @livewireStyles
 
 </head>
@@ -33,7 +34,7 @@
 
     {{-- Toast Message --}}
     @session('toast-success')
-        <x-toast.success :message="session('toast-success')" />
+    <x-toast.success :message="session('toast-success')" />
     @endsession
 
     <!--   Core JS Files   -->
@@ -68,11 +69,11 @@
     <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
     <script src="{{ asset('assets/js/soft-ui-dashboard.js') }}"></script>
     @session('toast-success')
-        <script>
-            var myToastEl = document.getElementById('toastSuccess');
+    <script>
+        var myToastEl = document.getElementById('toastSuccess');
             var myToast = new bootstrap.Toast(myToastEl); // Returns a Bootstrap toast instance
             myToast.show();
-        </script>
+    </script>
     @endsession
 
     @livewireScripts
